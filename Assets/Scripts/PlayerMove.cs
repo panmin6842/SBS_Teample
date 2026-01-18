146b3886713687d1cf3c75829bb8b5176ba381fd
panmin6842 <panmin6842@naver.com>
2026-01-18 10:01:20 +0900

    first commit
    플레이어 조작 제작

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    float moveSpeed = 5;
+
+    Vector2 movement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xOffset = movement.x * moveSpeed * Time.deltaTime;
+        float yOffset = movement.y * moveSpeed * Time.deltaTime;
+        transform.localPosition += new Vector3(xOffset, 0f, yOffset);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+}
