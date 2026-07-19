2ab8f2b1496a71156cd66a2102ef26b33537e0e4
thak5359 <soma53597@gmail.com>
2026-07-19 22:43:47 +0900

    Add : TurretEnemy 프리팹 생성, 고정 사격 B타입 완성
    TODO : 조준 사격 완성, 조준 사격 Decal 애님 완성

--- Assets/JB/Script/Runtime/AttackState.cs
+++ Assets/JB/Script/Runtime/AttackState.cs
@@ -13,12 +13,14 @@ public class AttackState : StateBase
     private CancellationTokenSource cts;
     private Animator animator;
     private int attackNumber = 0;
+    private bool isTurret = false;
 
     public AttackState(EnemyBase enemy) : base(enemy)
     {
         this.enemy = enemy;
         this.attackBehavior = enemy.attackBehavior;
         this.animator = enemy.GetComponentInChildren<Animator>();
+        this.isTurret = enemy is TurretEnemy;
     }
     public override async UniTask Enter(CancellationToken token)
     {
@@ -36,7 +38,7 @@ public class AttackState : StateBase
         attackNumber = UnityEngine.Random.Range(0, enemy.totalRatioOfAttacks);
         try
         {
-            await AttackAnim(cts.Token);
+            await AttackAnim(cts.Token, isTurret);
             if (!cts.Token.IsCancellationRequested)
             {
                 attackBehavior.Attack(attackNumber);
@@ -61,43 +63,53 @@ public class AttackState : StateBase
         this.animator.ResetTrigger("Attack");
         await UniTask.CompletedTask;
     }
-    protected virtual async UniTask AttackAnim(CancellationToken token)
+    protected virtual async UniTask AttackAnim(CancellationToken token, bool isTurret = false)
     {
         agent.isStopped = true;
         float timeSpent = 0f;
+        DecalProjector[] decals = enemyObject.GetComponentsInChildren<DecalProjector>();
 
         Debug.Log("<color=red>" + enemyObject.name + " Prepares to Attack!</color>");
 
-        DecalProjector[] decals = enemyObject.GetComponentsInChildren<DecalProjector>();
-        DecalProjector centerDecal = decals[1];
-
-        if (attackBehavior.GetIsMultiProjectileAttack(attackNumber))
+        if (isTurret)
         {
-            foreach (var item in decals)
+            switch (attackNumber)
             {
-                if (item != null) item.enabled = true;
+                case 0:
+                case 1:
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        if (decals[i] != null) decals[i].enabled = true;
+                    }
+                    break;
+                case 2:
+                case 3:
+                    for (int i = 4; i <= 7; i++)
+                    {
+                        if (decals[i] != null) decals[i].enabled = true;
+                    }
+                    break;
+                case 4:
+                    // if (decals[1] != null) decals[1].enabled = true;
+                    break;
             }
         }
         else
-            if (centerDecal != null) centerDecal.enabled = true;
+        {
+            TurnOnDecalsWhenNotTurret();
+        }
 
         try
         {
             while (timeSpent < 0.5f && !token.IsCancellationRequested)
             {
                 if (enemy.player == null || enemyObject == null) break;
-
-                Vector3 directionToPlayer = (enemy.player.position - enemyObject.transform.position).normalized;
-                directionToPlayer.y = 0f; // 수직 방향 무시
-
-                Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
-                // 적이 플레이어를 바라보도록 회전(시간변화량 * 속도)
-                enemyObject.transform.rotation = Quaternion.Slerp(enemyObject.transform.rotation, targetRotation, Time.deltaTime * 10f);
+                if(!isTurret) LookAtPlayer();
                 timeSpent += Time.deltaTime;
                 await UniTask.Yield(PlayerLoopTiming.Update, token);
             }
         }
-        finally
+        finally // 데칼, 에이전트, 작업 완료 상태를 처리 및 초기화
         {
             if (token.IsCancellationRequested) await UniTask.CompletedTask;
             if (enemyObject != null && decals != null)
@@ -109,6 +121,31 @@ public class AttackState : StateBase
             {
                 agent.isStopped = false;
             }
+        }
+
+        void TurnOnDecalsWhenNotTurret()
+        {
+            DecalProjector centerDecal = decals[1];
+
+            if (attackBehavior.GetIsMultiProjectileAttack(attackNumber))
+            {
+                foreach (var item in decals)
+                {
+                    if (item != null) item.enabled = true;
+                }
+            }
+            else
+                if (centerDecal != null) centerDecal.enabled = true;
+        }
+
+        void LookAtPlayer()
+        {
+            Vector3 directionToPlayer = (enemy.player.position - enemyObject.transform.position).normalized;
+            directionToPlayer.y = 0f; // 수직 방향 무시
+
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            // 적이 플레이어를 바라보도록 회전(시간변화량 * 속도)
+            enemyObject.transform.rotation = Quaternion.Slerp(enemyObject.transform.rotation, targetRotation, Time.deltaTime * 10f);
         }
     }
 
--- Assets/JB/Script/Runtime/Turret/TurretAttack.cs
+++ Assets/JB/Script/Runtime/Turret/TurretAttack.cs
@@ -17,12 +17,19 @@ public class TurretAttack : IAttackBehavior
     {
         Debug.Log("TypeA Shot");
         GameObject obj = GameObject.Instantiate(turretPrefabs[0], enemy.transform.position, enemy.transform.rotation);
-        obj.transform.parent = enemy.transform;
     }
 
     public void FixedShotTypeB()
     {
         Debug.Log("TypeB Shot");
+        GameObject obj = GameObject.Instantiate(turretPrefabs[0], enemy.transform.position, enemy.transform.rotation);
+        obj.transform.rotation = obj.transform.rotation * Quaternion.Euler(0, 45, 0);
+    }
+
+    public void SnipingShot()
+    {
+        Debug.Log("Sniping Shot");
+        GameObject obj = GameObject.Instantiate(turretPrefabs[1], enemy.transform.position, enemy.transform.rotation);
     }
 
     public void Attack(int randomAttack)
@@ -30,14 +37,19 @@ public class TurretAttack : IAttackBehavior
         switch (randomAttack)
         {
             case 0:
+                FixedShotTypeA();
                 break;
             case 1:
+                FixedShotTypeA();
                 break;
             case 2:
+                FixedShotTypeB();
                 break;
             case 3:
+                FixedShotTypeB();
                 break;
             case 4:
+                SnipingShot();
                 break;
         }
     }
